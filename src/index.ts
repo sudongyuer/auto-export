5b350c699d848694433599b8530a2e962593423b
sudongyu <976499226@qq.com>
2022-07-10 17:49:34 +0800

    chore: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,63 +2,77 @@ import { cwd } from 'process'
 import fs from 'fs'
 import path from 'path'
 import chalk from 'chalk'
-import { tryRequire } from './utils'
 
-const configArray = tryRequire('./export.config', cwd())
+import { loadConfig } from 'unconfig'
 
-configArray.forEach((config: config) => {
-  const { targetFilesDir, outputFileDir, customImport } = config
-  if (!targetFilesDir || !outputFileDir) {
-    console.error(chalk.bgRedBright('Please check the export.config.ts'))
-    process.exit(-1)
-  }
-  let generateStr = ''
-  if (customImport) {
-    let files = fs.readdirSync(path.resolve(cwd(), targetFilesDir))
-    files = files.filter((file) => {
-      return (file !== 'index.ts' && file !== '.DS_Store')
-    })
-    const fileNameArray = files.map((file) => {
-      return transformFileName(file)
-    })
-    files.forEach((file) => {
-      if (file !== 'index.ts') {
-        const fileName = transformFileName(file)
-        generateStr += `${customImport(fileName, file)}\n`
-      }
-    })
-    const exportStr = `export {
-${fileNameArray.join(',\n')}
+async function main() {
+  const { config: configArray } = await loadConfig<ExportConfig>({
+    sources: [
+      {
+        files: 'export.config',
+        extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json', ''],
+      },
+    ],
+  })
+  configArray?.configs.forEach((config: config) => {
+    const { targetDir, outputDir = targetDir, customImport } = config
+    if (!targetDir) {
+      console.error(chalk.bgRedBright('Please check the export.config.ts'))
+      process.exit(-1)
     }
-    `
-    generateStr += exportStr
-    fs.writeFileSync(path.resolve(cwd(), `${outputFileDir}/index.ts`), generateStr)
-    console.warn(chalk.bgCyan(`🦥 大哥${path.resolve(cwd(), `${outputFileDir}/index.ts`)}生成完毕，小弟撤退了🚗~`))
-  }
-  else {
-    let files = fs.readdirSync(path.resolve(cwd(), targetFilesDir))
-    files = files.filter((file) => {
-      return (file !== 'index.ts' && file !== '.DS_Store')
-    })
-
-    const fileNameArray = files.map((file) => {
-      return transformFileName(file)
-    })
-    files.forEach((file) => {
-      if (file !== 'index.ts') {
-        const fileName = transformFileName(file)
-        generateStr += `import ${fileName} from '${file}'\n`
-      }
-    })
-    const exportStr = `export {
-${fileNameArray.join(',\n')}
+    let generateStr = ''
+    if (customImport) {
+      let files = fs.readdirSync(path.resolve(cwd(), targetDir))
+      files = files.filter((file) => {
+        return (file !== 'index.ts' && file !== '.DS_Store')
+      })
+      const fileNameArray = files.map((file) => {
+        return transformFileName(file)
+      })
+      files.forEach((file) => {
+        if (file !== 'index.ts') {
+          const fileName = transformFileName(file)
+          generateStr += `${customImport(fileName, file)}\n`
+        }
+      })
+      const exportStr = `
+export {
+${fileNameArray.map(item => `  ${item}`).join(',\n')},
+}
+`
+      generateStr += exportStr
+      fs.writeFileSync(path.resolve(cwd(), `${outputDir}/index.ts`), generateStr)
+      console.warn(chalk.bgBlue(`🚀🚀🚀 ${path.resolve(cwd(), `${outputDir}/index.ts`)}生成完毕，小弟撤退了🚗~`))
     }
-    `
-    generateStr += exportStr
-    fs.writeFileSync(path.resolve(cwd(), `${outputFileDir}/index.ts`), generateStr)
-    console.warn(chalk.bgCyan(`🦥 大哥${path.resolve(cwd(), `${outputFileDir}/index.ts`)}生成完毕，小弟撤退了🚗~`))
-  }
-})
+    else {
+      let files = fs.readdirSync(path.resolve(cwd(), targetDir))
+      files = files.filter((file) => {
+        return (file !== 'index.ts' && file !== '.DS_Store')
+      })
+
+      const fileNameArray = files.map((file) => {
+        return transformFileName(file)
+      })
+      files.forEach((file) => {
+        if (file !== 'index.ts') {
+          const fileName = transformFileName(file)
+          generateStr += `import ${fileName} from '${file}'\n`
+        }
+      })
+      const exportStr = `
+export {
+${fileNameArray.map(item => `  ${item}`).join(',\n')},
+}
+`
+      generateStr += exportStr
+      fs.writeFileSync(path.resolve(cwd(), `${outputDir}/index.ts`), generateStr)
+      console.warn(chalk.bgBlue(`🚀🚀🚀 ${path.resolve(cwd(), `${outputDir}/index.ts`)}生成完毕，小弟撤退了🚗~`))
+    }
+  })
+  process.exit(0)
+}
+
+main()
 
 function transformFileName(str: string) {
   str = str.slice(0, str.indexOf('.'))
@@ -71,13 +85,15 @@ function transformFileName(str: string) {
 }
 
 interface config {
-  targetFilesDir: string
-  outputFileDir?: string
+  targetDir: string
+  outputDir?: string
   customImport?: (fileName: string, file: string) => string
 }
-interface ExportConfig {
-  [index: number]: config
+
+export interface ExportConfig {
+  configs: Array<config>
 }
+
 function defineExportConfig(config: ExportConfig): ExportConfig {
   return config
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,45 @@
+import path from 'node:path'
 import jiti from 'jiti'
+import type { ExportConfig } from '.'
+
 export function tryRequire(id: string, rootDir: string = process.cwd()) {
-  const _require = jiti(rootDir, { interopDefault: true })
+  const _require = jiti(rootDir, {
+    interopDefault: true,
+    cache: false,
+    requireCache: false,
+    v8cache: false,
+    esmResolve: true,
+  })
   try {
     return _require(id)
   }
   catch (err: any) {
-    if (err.code !== 'MODULE_NOT_FOUND') {
+    if (err.code !== 'MODULE_NOT_FOUND')
       console.error(`Error trying import ${id} from ${rootDir}`, err)
-      console.error('Please make export.config.ts in your rootDir')
-    }
 
     return {}
   }
+}
+
+export function tryResolve(id: string, rootDir: string = process.cwd()) {
+  const _require = jiti(rootDir, {
+    interopDefault: true,
+    cache: false,
+    requireCache: false,
+    v8cache: false,
+    esmResolve: true,
+  })
+  try {
+    return _require.resolve(id)
+  }
+  catch (err: any) {
+    if (err.code !== 'MODULE_NOT_FOUND')
+      console.error(`Error trying import ${id} from ${rootDir}`, err)
+    return id
+  }
+}
+
+export async function tryImport(id: string, rootDir: string = process.cwd()): Promise<ExportConfig> {
+  const { default: exportConfig } = await import(path.resolve(rootDir, id))
+  return exportConfig
 }
