e63a1f833e873f292f9e9ea9359859b4ba906098
Frozen FIsh <976499226@qq.com>
2022-07-16 17:34:06 +0800

    feat: add autoPrefix option

---
+++ shim.d.ts
@@ -0,0 +1,31 @@
+declare module '*.svg' {
+  import * as React from 'react'
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
+
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
+declare module '*.json' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpeg' {
+  const content: string
+  export default content
+}
+
+declare module '*.gif' {
+  const content: string
+  export default content
+}
