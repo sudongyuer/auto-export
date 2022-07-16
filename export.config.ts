a68eeb112d92860df7f57b9ee6ae91fe02f15025
Frozen FIsh <976499226@qq.com>
2022-07-16 17:32:04 +0800

    chore: update export.config.ts

--- export.config.ts
+++ export.config.ts
@@ -1,10 +1,25 @@
-export default {
+import { defineExportConfig } from './src'
+
+export default defineExportConfig({
   configs: [
     {
-      targetDir: "./src/images",
-      customImport: (fileName: string, file: string) => {
-        return `import {${fileName} as ${fileName}a} from '${file}'`
-      }
+      targetDir: './src/images/',
+      depth: true,
+      autoPrefix: true,
+    },
+    {
+      targetDir: './src/icon/',
+      depth: true,
+      autoPrefix: true,
+    },
+    {
+      targetDir: './src/svgs/',
+      depth: true,
+      autoPrefix: true,
+      customImport: (fileName, file, fileType) => {
+        return `import { ReactComponent as ${fileType}${fileName} } from '${file}'`
+      },
     },
   ],
-};
+})
+
